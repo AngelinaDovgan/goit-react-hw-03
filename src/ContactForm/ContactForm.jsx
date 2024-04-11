f69d32fe24dba886ecbcf0911574a5c6effb4b60
Angelina <midnightshout03@gmail.com>
2024-04-11 13:53:33 +0300

    7+css

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import { Field, Formik, Form, ErrorMessage } from "formik";
 import { object, string, number } from 'yup';
+import css from './ContactForm.module.css'
 
 const UserSchema = object({
     name: string().min(3, "Type at least 3 characters!").max(50, "Max number of characters is 50!").required("Is required!"),
@@ -20,15 +21,15 @@ export default function ContactForm({ onAdd }) {
                 });
                 resetForm();
             }}>
-            <Form>
+            <Form className={css.frm}>
                 <label>Name</label>
                 <Field type="text" name="name" />
-                <ErrorMessage name="name" component="span"/>
+                <ErrorMessage name="name" component="span" className={css.span}/>
                 
                 <label>Number</label>
                 <Field type="number" name="number" />
-                <ErrorMessage name="number" component="span"/>
-            <button type="submit">Add contact</button>
+                <ErrorMessage name="number" component="span" className={css.span}/>
+            <button type="submit" className={css.btn}>Add contact</button>
         </Form> 
         </Formik>
         
--- src/ContactList/ContactList.jsx
+++ src/ContactList/ContactList.jsx
@@ -1,10 +1,12 @@
 import Contact from "../Contact/Contact"
+import css from './ContactList.module.css'
+
 export default function ContactList({ list, onDelete }) {
     return (
     
-        <ul>
+        <ul className={css.mainlist}>
             {list.map((item) => (
-                        <li key={item.id}>
+                        <li key={item.id} className={css.listitem}>
                         <Contact
                         data={item}
                         name={item.name}
