a1e7e8bd5895017056cffa9a5d5ab3a192ab1fdf
Angelina <midnightshout03@gmail.com>
2024-04-12 09:56:35 +0300

    7

--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,19 +1,27 @@
 import { Field, Formik, Form, ErrorMessage } from "formik";
+import { useId } from "react";
 import { object, string, number } from 'yup';
 import css from './ContactForm.module.css'
+
 
 const UserSchema = object({
     name: string().min(3, "Type at least 3 characters!").max(50, "Max number of characters is 50!").required("Is required!"),
     number: number().required("Is required!")
 })
-
-
 export default function ContactForm({ onAdd }) {
+const nameId = useId();
+const numberId = useId();
+
+const initialValues = {
+name: "",
+number: ""
+};
+    
     return (
         <Formik
-            initialValues={{ name: '', number: '' }}
+            initialValues={initialValues}
             validationSchema={UserSchema}
-            onSubmit={(values, { resetForm }) => {
+            onSubmit= {(values, { resetForm }) => {
                 onAdd({
                     id: Date.now(),
                     name: values.name,
@@ -22,13 +30,18 @@ export default function ContactForm({ onAdd }) {
                 resetForm();
             }}>
             <Form className={css.frm}>
-                <label>Name</label>
-                <Field type="text" name="name" />
-                <ErrorMessage name="name" component="span" className={css.span}/>
+                <div className={css.group}>
+                 <label htmlFor={nameId}>Name</label>
+                    <Field type="text" name="name" id={nameId} />
+                <ErrorMessage name="name" component="span" className={css.span}/>   
+                </div>
                 
-                <label>Number</label>
-                <Field type="number" name="number" />
-                <ErrorMessage name="number" component="span" className={css.span}/>
+                <div className={css.group}>
+                   <label htmlFor={numberId}>Number</label>
+                    <Field type="number" name="number" id={numberId} />
+                <ErrorMessage name="number" component="span" className={css.span}/>  
+                </div>
+               
             <button type="submit" className={css.btn}>Add contact</button>
         </Form> 
         </Formik>
