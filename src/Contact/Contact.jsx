794b98162a788079e8828cba87507dd5d228fdb9
Angelina <midnightshout03@gmail.com>
2024-04-12 10:20:47 +0300

    9

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,13 @@
 import { FaUser } from "react-icons/fa";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import css from './Contact.module.css'
 
 export default function Contact({ data: { name, number, id }, onDelete }) {
     return (
         <>
            
-            <p> <FaUser /> {name}</p>
-            <p> <BsFillTelephoneFill /> {number}</p>
+            <p className={css.ctext}> <FaUser /> {name}</p>
+            <p className={css.ctext}> <BsFillTelephoneFill /> {number}</p>
             <button onClick={() => onDelete(id)}>
             Delete</button>
         </>
